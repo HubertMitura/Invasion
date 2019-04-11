b46baf7231fca628c87eb64ac636450afc7d4407
Hubert Mitura <hubertmitura83@gmail.com>
2019-04-11 18:55:28 +0200

    Score progression added

--- settings.py
+++ settings.py
@@ -20,6 +20,7 @@ class Settings():
 		self.bullets_allowed = 3
 
 		self.speedup_scale = 1.1
+		self.score_scale = 1.5
 
 		self.initialize_dynamic_settings()
 
@@ -35,6 +36,9 @@ class Settings():
 		self.ship_speed_factor *= self.speedup_scale 
 		self.bullet_speed_factor *= self.speedup_scale
 		self.alien_speed_factor *= self.speedup_scale
+
+		self.alien_points = int(self.alien_points * self.score_scale)
+		
 
 
 
